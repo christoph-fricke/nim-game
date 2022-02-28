b87b7e4ec30a64e682da3b3588bd868dbe3a6958
Christoph Fricke <christoph@frickeonline.de>
2022-02-28 16:16:26 +0100

    feat(players): implement human player actor

--- src/human-player/human-player-machine.ts
+++ src/human-player/human-player-machine.ts
@@ -1,5 +1,11 @@
-import { createMachine, t } from "xstate";
-import type { PlayerEvent } from "../game-manager";
+import { assign, createMachine, sendParent, t } from "xstate";
+import { PlayerEvent, playMove, stopGame } from "../game-manager";
+import { createMove, getFreePositions, maxAllowed, Move } from "../nim";
+import {
+  getInitialContext,
+  HumanContext,
+  HumanEvent,
+} from "./human-player-machine.model";
 
 export interface HumanPlayerDependencies {
   /** The `secret` is provided by the game manager to verify moves from this actor. */
@@ -7,12 +13,93 @@ export interface HumanPlayerDependencies {
 }
 
 export function createHumanPlayerMachine(deps: HumanPlayerDependencies) {
-  return createMachine({
-    tsTypes: {} as import("./human-player-machine.typegen").Typegen0,
-    schema: {
-      context: t<{}>(),
-      events: t<PlayerEvent>(),
+  return createMachine(
+    {
+      tsTypes: {} as import("./human-player-machine.typegen").Typegen0,
+      schema: {
+        context: t<HumanContext>(),
+        events: t<PlayerEvent | HumanEvent>(),
+      },
+      id: "HumanPlayer",
+      context: getInitialContext(),
+      initial: "AwaitingRequest",
+      on: {
+        "human.stop_game": { internal: true, actions: "proxyGameStop" },
+      },
+      states: {
+        AwaitingRequest: {
+          on: {
+            "game.moves.request": {
+              target: "Playing",
+              actions: "saveGameState",
+            },
+          },
+        },
+        Playing: {
+          initial: "NonSelected",
+          entry: "clearMove",
+          onDone: "AwaitingResponse",
+          states: {
+            NonSelected: {
+              on: {
+                "human.toggle_match": {
+                  target: "MoveSelected",
+                  cond: "validPositionAndMoveNotFull",
+                  actions: "updateMove",
+                },
+              },
+            },
+            MoveSelected: {
+              on: {
+                "human.submit": "Submit",
+                "human.toggle_match": [
+                  {
+                    target: "NonSelected",
+                    cond: "willEmptyMove",
+                    actions: "updateMove",
+                  },
+                  {
+                    target: "MoveSelected",
+                    cond: "validPositionAndMoveNotFull",
+                    actions: "updateMove",
+                  },
+                ],
+              },
+            },
+            Submit: { type: "final", entry: "respondWithMove" },
+          },
+        },
+        AwaitingResponse: {
+          on: {
+            "game.moves.accept": "AwaitingRequest",
+            "game.moves.decline": "Playing",
+          },
+        },
+      },
     },
-    id: "HumanPlayer",
-  });
+    {
+      guards: {
+        validPositionAndMoveNotFull: (c, e) =>
+          c.freePos.includes(e.position) &&
+          (c.nextMove.includes(e.position) || c.nextMove.length < maxAllowed),
+        willEmptyMove: (c, e) =>
+          c.nextMove.length === 1 && c.nextMove.includes(e.position),
+      },
+      actions: {
+        proxyGameStop: sendParent(stopGame()),
+        saveGameState: assign((c, e) => ({
+          pile: e.pile,
+          freePos: getFreePositions(e.pile),
+        })),
+        clearMove: assign({ nextMove: (c) => [] as unknown as Move }),
+        updateMove: assign({
+          nextMove: (c, e) =>
+            c.nextMove.includes(e.position)
+              ? createMove(c.nextMove.filter((p) => p !== e.position))
+              : createMove(c.nextMove.concat(e.position)),
+        }),
+        respondWithMove: sendParent((c) => playMove(deps.secret, c.nextMove)),
+      },
+    }
+  );
 }
