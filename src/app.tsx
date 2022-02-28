80327d70dfdf55bddf5e8c77f489fab53be1cd2b
Christoph Fricke <christoph@frickeonline.de>
2022-02-28 16:46:49 +0100

    feat: add basic screen placeholder for UI feedback

--- src/app.tsx
+++ src/app.tsx
@@ -16,14 +16,51 @@ export function App() {
     }
   );
 
-  const isMainMenu = useSelector(gameManager, (state) =>
-    state.hasTag("main_menu")
-  );
+  const screen = useSelector(gameManager, (state) => {
+    switch (true) {
+      case state.hasTag("human_won"):
+        return "human_won";
+      case state.hasTag("human_lost"):
+        return "human_lost";
+      case state.hasTag("playing"):
+        return "playing";
+      default:
+        return "main_menu";
+    }
+  });
 
-  if (isMainMenu) {
+  if (screen === "main_menu") {
     return (
       <>
         <h1 className={styles.heading}>Nim Game</h1>
+        <button
+          className={styles.button}
+          onClick={() => gameManager.send(startGame())}
+        >
+          Start Game
+        </button>
+      </>
+    );
+  }
+
+  if (screen === "human_won") {
+    return (
+      <>
+        <h1 className={styles.heading}>You Won</h1>
+        <button
+          className={styles.button}
+          onClick={() => gameManager.send(startGame())}
+        >
+          Start Game
+        </button>
+      </>
+    );
+  }
+
+  if (screen === "human_lost") {
+    return (
+      <>
+        <h1 className={styles.heading}>You Lost</h1>
         <button
           className={styles.button}
           onClick={() => gameManager.send(startGame())}
